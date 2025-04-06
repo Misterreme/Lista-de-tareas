0475319fbccf087f5e3a867f7f24e011bdc12262
Misterreme <nunezemmanuel107@gmail.com>
2025-04-06 17:36:16 -0400

    Cambio de diseño

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,47 @@
-const taskList = document.getElementById("taskList");
-const inputTask = document.getElementById("taskInput");
-const addButton = document.getElementById("addTaskButton");
-const errorMessage = document.getElementById("errorMessage");
+const addButton = document.getElementById("add-button");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+const errorMessage = document.getElementById("error-message");
 
-addButton.addEventListener("click", () => {
-    if (inputTask.value.trim() === "") {
-        errorMessage.textContent = "No puedes dejar este campo vacio";
-        errorMessage.style.display = "block";
-        return; 
-    }
-
-    if (taskList.children.length === 6) {
-        errorMessage.textContent = "¡Maximo de tareas alcanzado!";
-        errorMessage.style.display = "block";
-        return;
-    }
-    
-    const liElement = document.createElement("li");
-    liElement.textContent = inputTask.value;
+const defaultLiElement = document.getElementById("firts-element");
+const removeFirstElement = document.getElementById("remove-button");
+removeFirstElement.addEventListener("click", () => {
+    taskList.lastElementChild.remove();
     errorMessage.style.display = "none"
-    
+})
+
+addButton.addEventListener("click", (event) => {
+    // console.log(taskInput.value);
+
+    if (taskInput.value.trim() === "") {
+        console.error("ERRORRRR");
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "This field must not be empty";
+        return 
+    } 
+
+    if (taskList.children.length === 8) {
+        errorMessage.textContent = "Maximum number of tasks reached";
+        errorMessage.style.display = "block";
+        return
+    }
+
+    const liElement = document.createElement("li");
+    liElement.textContent = taskInput.value;
+    errorMessage.style.display = "none";
+
+    console.log(taskList.children.length);
+
     const removeBtn = document.createElement("button");
-    removeBtn.textContent = "X";
-    removeBtn.setAttribute("class", "removeBtn");
+    removeBtn.textContent = "X"
+    removeBtn.setAttribute("class", "remove-button");
     removeBtn.addEventListener("click", () => {
         taskList.lastElementChild.remove();
         errorMessage.style.display = "none"
     })
-    
-    liElement.append(removeBtn);
-    taskList.append(liElement);
 
-    inputTask.value = ""
+    taskList.append(liElement);
+    liElement.append(removeBtn);
+
+    taskInput.value = ""
 })
